13ce5ebdcde9d90fb671f35be6a6236184d12715
FHL-GITHUB <55864600+FHL-GITHUB@users.noreply.github.com>
2020-10-30 01:59:30 +0800

    #To test AdminMgr

---
+++ ruizhi-templete/src/com/ntustars/controller/TesterLALALALALA.java
@@ -0,0 +1,18 @@
+package com.ntustars.controller;
+
+public class TesterLALALALALA {   
+
+    public static void main(String[] args) throws IOException{     
+    	
+        AdminMgr admin = new AdminMgr();
+        //admin.updStudParticulars();
+        //admin.printAllStudent();
+        
+        //admin.printStudListByIndex();
+        //admin.printStudListByCourse();
+        
+        //admin.addStudInfo();
+        
+    } 
+
+}
