7bb4746abd7701c80aa055f14b05430788958cac
FHL-GITHUB <55864600+FHL-GITHUB@users.noreply.github.com>
2020-10-31 10:42:49 +0800

    Update AccessPeriod.java

--- ruizhi-templete/src/com/ntustars/entity/AccessPeriod.java
+++ ruizhi-templete/src/com/ntustars/entity/AccessPeriod.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class AccessPeriod {
-	 	private Date startDateTime;
+	    private Date startDateTime;
 	    private Date endDateTime;
 	    public SimpleDateFormat formatter;
 	    private ArrayList<String> accessPeriod;
@@ -34,6 +34,7 @@ public class AccessPeriod {
 	    }
 	    
 	    public ArrayList<String> getAccessPeriod() {
+		this.accessPeriod.clear();
 	    	this.accessPeriod.add(this.getStartAccessDate());
 	    	this.accessPeriod.add(this.getEndAccessDate());
 	    	return this.accessPeriod;
