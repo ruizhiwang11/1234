bbea7ad18e7cb48b473689eb86337badd918e4ce
yiheng123 <yiheng@nutonomy.com>
2020-11-01 20:34:14 +0800

    c

--- ruizhi-templete/src/com/ntustars/controller/StudentMrg.java
+++ ruizhi-templete/src/com/ntustars/controller/StudentMrg.java
@@ -3,6 +3,7 @@ import com.ntustars.entity.Course;
 import com.ntustars.entity.Course.CourseType;
 import com.ntustars.entity.CourseCompoment;
 import com.ntustars.entity.CourseInfo;
+import com.ntustars.entity.Student;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -11,13 +12,39 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 import java.util.Map;
 
+// student course table: StID, courseID, courseID, courseID, courseID, etc ...
+
 
 public class StudentMrg {
 
-    private HashMap<String, String, Boolean> returnData;
+    private TxtReaderWriter txtReaderWriter;
+    private int vacancy;
+    private StudentMrg studentMrg;
+    public HashMap<String, ArrayList<String >> studnetCoursetable;
+    private ArrayList studentCourse;
+
+    //private HashMap<String, String, Boolean> returnData;
+    public StudentMrg() throws IOException{
+        txtReaderWriter = new TxtReaderWriter();
+
+    }
+    private readStudentCourseTable( String studentid){
+        HashMap studentCourseMap = (HashMap)txtReaderWriter.readtxt("Student Course Table.txt");
+        if (studentCourseMap.containsKey(studentid) == false){
+            studentCourseMap.put(studentid, "");
+            return studentCourseMap.value(studentid);
+        }
+        else {
+            return studentCourseMap.value(studentid);
+            
+        }
+    }
+
 
     public boolean studentAddCourse(String StudenId, int courseId){
         boolean success = false;
+        studentCourse = new ArrayList<>();
+        if ()
         //HashMap returnData = new HashMap<String, String, Boolean>(StudenId, courseId, success);
 
         //call addCourse
