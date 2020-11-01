bef06a3ac8d0c1b18dea83b2872f66fedf39815e
yiheng123 <yiheng@nutonomy.com>
2020-11-01 19:28:06 +0800

    asd

--- ruizhi-templete/src/com/ntustars/controller/StudentMrg.java
+++ ruizhi-templete/src/com/ntustars/controller/StudentMrg.java
@@ -1,5 +1,74 @@
 package com.ntustars.controller;
+import com.ntustars.entity.Course;
+import com.ntustars.entity.Course.CourseType;
+import com.ntustars.entity.CourseCompoment;
+import com.ntustars.entity.CourseInfo;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.Map;
 
 
 public class StudentMrg {
+
+    private HashMap<String, String, Boolean> returnData;
+
+    public boolean studentAddCourse(String StudenId, int courseId){
+        boolean success = false;
+        //HashMap returnData = new HashMap<String, String, Boolean>(StudenId, courseId, success);
+
+        //call addCourse
+        //check success of addCourse if success success
+
+        return success;
+    }
+    public boolean studentDropCourse(String StudenId, int courseId){
+        boolean success = false;
+        //HashMap returnData = new HashMap<String, String, Boolean>(StudenId, courseId, success);
+
+        //call addCourse
+        //check success of addCourse if success success
+
+        return success;
+    }
+    public boolean studentCheckCourse(String StudenId, int courseId){
+        boolean success = false;
+        //HashMap returnData = new HashMap<String, String, Boolean>(StudenId, courseId, success);
+
+        //call addCourse
+        //check success of addCourse if success success
+
+        return success;
+    }
+    public boolean studentCheckVacanCourse(String StudenId, int courseId){
+        boolean success = false;
+        //HashMap returnData = new HashMap<String, String, Boolean>(StudenId, courseId, success);
+
+        //call addCourse
+        //check success of addCourse if success success
+
+        return success;
+    }
+    public boolean studentCourseIndex(String StudenId, int courseId){
+        boolean success = false;
+        //HashMap returnData = new HashMap<String, String, Boolean>(StudenId, courseId, success);
+
+        //call addCourse
+        //check success of addCourse if success success
+
+        return success;
+    }
+    public boolean studentSwopCourse(String StudenId, int courseId){
+        boolean success = false;
+        //HashMap returnData = new HashMap<String, String, Boolean>(StudenId, courseId, success);
+
+        //call addCourse
+        //check success of addCourse if success success
+
+        return success;
+    }
+
 }
